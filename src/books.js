910fba1fe2658ba6321a43210e7eefe1e5a3b041
SU-T <guitaristpeet@gmail.com>
2022-08-12 13:35:17 -0700

    add books data

---
+++ src/books.js
@@ -0,0 +1,67 @@
+export const books = [
+  {
+    id: 1,
+    name: 'Dark Island',
+    author: 'Stephen Cross',
+    price: '25.99',
+    desc: 'A new island holiday resort opens. Within days, two families are found dead, their faces frozen in terror. Was what they saw enough to kill them?',
+    img: './assets/IMG-1.jpg'
+  },
+  {
+    id: 2,
+    name: 'The Wanders',
+    author: 'JB Murray',
+    price: '17.99',
+    desc: 'Dark magic, an unrelenting demon, and alternate history meet modern day in a tale for fans of classic horror literature.',
+    img: './assets/IMG-2.jpg'
+  },
+  {
+    id: 3,
+    name: 'Ask No Questions, Hear No Delusions',
+    author: 'Gerry Cav',
+    price: '14.99',
+    desc: 'We are all capable of immorality, including murder, whether we want to believe this or not. It just takes the right amount of provocation in the wrong circumstances. It’s what you do next that really defines you and whatever lies it takes to delude yourself from reality.',
+    img: './assets/IMG-3.jpg'
+  },
+  {
+    id: 4,
+    name: "Devil's Messenger",
+    author: 'Randy Turner',
+    price: '16.99',
+    desc: 'Some people just won’t stay dead – Only a year after Cassandra Harper found her father murdered and slumped over the computer he gave her for her birthday, she begins receiving messages from him on that same computer As Cassandra tries to find her father’s killer, she slowly begins to wonder just what kind of a man he was and what kind of evil was he planning. And will she be able to stop him and his killer before she becomes the next victim? Devil’s Messenger is a roller coaster ride of horror that you won’t be able to put down. Grab your copy today!',
+    img: './assets/IMG-4.jpg'
+  },
+  {
+    id: 5,
+    name: 'The Blood of the Lion: The Vorelian Saga',
+    author: 'C.D. McKenna',
+    price: '31.99',
+    desc: 'The Demon Killer, once responsible for the Diyrặllian Massacre, has been found after being lost for centuries. Millions of lives fell victim to this treacherous relic . . . and now Syra holds it. Torn from the sanctuary of her home, she must learn to live on the run in search for freedom, hunted by forces known and unknown.',
+    img: './assets/IMG-5.jpg'
+  },
+  {
+    id: 6,
+    name: 'Hide',
+    author: 'Kiersten White',
+    price: '14.99',
+    desc: 'A high-stakes hide-and-seek competition turns deadly in this dark supernatural thriller from New York Times bestselling author Kiersten White.',
+    img: './assets/IMG-6.jpg'
+  },
+  {
+    id: 7,
+    name: 'Ascendancy',
+    author: 'S.R. Miller',
+    price: '7.99',
+    desc: 'It’s one hell of a story. But will anyone live long enough to tell it?',
+    img: './assets/IMG-7.jpg'
+  },
+  {
+    id: 8,
+    name: 'The Haunting of Hill House',
+    author: 'Shirley Jackson',
+    price: '15.99',
+    desc: 'The greatest haunted house story ever written, the inspiration for a 10-part Netflix series directed by Mike Flanagan and starring Michiel Huisman, Carla Gugino, and Timothy Hutton',
+    img: './assets/IMG-8.jpg'
+  },
+];
+
